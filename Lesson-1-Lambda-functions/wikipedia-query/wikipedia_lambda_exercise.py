843be0af52e405e806987614c0c36ba8c384b004
EC2 Default User <ec2-user@ip-172-31-44-33.ec2.internal>
2021-06-07 13:41:11 +0000

    Refactored wikipedia-sam app

--- Lesson-1-Lambda-functions/wikipedia-query/wikipedia_lambda_exercise.py
+++ Lesson-1-Lambda-functions/wikipedia-query/wikipedia_lambda_exercise.py
@@ -13,10 +13,10 @@ def lambda_handler(event, context):
     
     ## TO DO: Check that the request has some input body and save it
     if 'body' in event:
-        body = None
+        body = json.loads(event['body'])
     
     ## TO DO: Get the wikipedia "entity" from the body of the request
-    entity = None
+    entity = body['entity']
     res = wikipedia.summary(entity, sentences=1) # first sentence, result
 
     # print statements
@@ -26,6 +26,13 @@ def lambda_handler(event, context):
     ## TO DO: Format the response as JSON and return the result
     response = {
         ## your code here
+        "StatusCode": 200,
+        "Headers": {
+            "Content-Type": "application/json",
+        },
+        "Body": {
+            "message": res
+        }
     }
     
     return response
